cecb51706270882faa6fb386f8af01a451f2c239
koromo <proggr0@gmail.com>
2015-12-28 04:07:25 +0900

    Add makeRange() function for container

---
+++ killmetech/src/core/range.h
@@ -0,0 +1,50 @@
+#ifndef _KILLME_RANGE_H_
+#define _KILLME_RANGE_H_
+
+namespace killme
+{
+    namespace detail
+    {
+        template <class It>
+        class IteratorRange
+        {
+        private:
+            It begin_;
+            It end_;
+
+        public:
+            IteratorRange(It begin, It end)
+                : begin_(begin)
+                , end_(end)
+            {
+            }
+
+            It begin() const
+            {
+                return begin_;
+            }
+
+            It end() const
+            {
+                return end_;
+            }
+        };
+    }
+
+    // Create range from iterator
+    template <class It>
+    detail::IteratorRange<It> makeRange(It begin, It end)
+    {
+        return detail::IteratorRange<It>(begin, end);
+    }
+
+    // Create range from container
+    template <class C>
+    auto makeRange(const C& c)
+        -> decltype(makeRange(std::cbegin(c), std::cend(c)))
+    {
+        return makeRange(std::cbegin(c), std::cend(c));
+    }
+}
+
+#endif
